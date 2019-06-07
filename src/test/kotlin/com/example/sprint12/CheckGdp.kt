051a5e67811a4d5180599b227e1d375cd22b07ed
BL-CoderOvertime <brandonlively@live.com>
2019-06-07 11:53:33 -0400

    Copied over and modified files from Previous project, GdpController is the only one left to go

--- src/test/kotlin/com/example/sprint12/CheckGdp.kt
+++ src/test/kotlin/com/example/sprint12/CheckGdp.kt
@@ -1,2 +1,8 @@
 package com.example.sprint12
 
+import com.example.sprint12.model.GDP
+
+interface CheckGdp {
+    fun test(g: GDP): Boolean
+
+}
--- src/test/kotlin/com/example/sprint12/model/GDP.kt
+++ src/test/kotlin/com/example/sprint12/model/GDP.kt
@@ -1,2 +1,25 @@
 package com.example.sprint12.model
 
+import java.util.concurrent.atomic.AtomicLong
+
+class GDP{
+    var id: Long = 0
+    var countryGdp: Long = 0
+    var name = ""
+
+    constructor(toClone: GDP) {
+        this.id = toClone.id
+        this.countryGdp = toClone.countryGdp
+        this.name = toClone.name
+    }
+
+    constructor(name: String,countryGdp: String) {
+        this.id = counter.incrementAndGet()
+        this.countryGdp = countryGdp.toLong()
+        this.name = name
+    }
+
+    companion object {
+        private val counter = AtomicLong()
+    }
+}
