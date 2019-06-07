681de9838c4e39b266a8b6ca00945a49760b189a
BL-CoderOvertime <brandonlively@live.com>
2019-06-07 14:37:37 -0400

    Stretch Goals of returning totalGDP and GDPrange is setup
    TODO: fix returning name range endpoint

--- src/main/kotlin/com/example/sprint12/controller/GdpController.kt
+++ src/main/kotlin/com/example/sprint12/controller/GdpController.kt
@@ -91,19 +91,68 @@ class GdpController{
 
         return mav
     }
-/*    // localhost:2019/total
+    // localhost:2019/total
     @RequestMapping(value = "/total")
     fun getTotalGDP():ResponseEntity<*>{
         logger.info("/total accessed")
+
+        var runningTotal:Long = 0
+        for(g:GDP in ourGdpList.gdpList){
+            runningTotal+=g.countryGdp
+        }
+
+        val rtnGdp:GDP = GDP("Total", runningTotal.toString())
+        return ResponseEntity(rtnGdp, HttpStatus.OK)
     }
-    // localhost:2019/names/{startLetter}/{endLetter}
+
+
+/*    // localhost:2019/names/{startLetter}/{endLetter}
     @RequestMapping(value = "/names/{startLetter}/{endLetter}")
     fun getByNameRange(@PathVariable startLetter: String, @PathVariable endLetter: String):ResponseEntity<*>{
         logger.info("/names/$startLetter/$endLetter accessed")
-    }
+
+        var sortedList = ourGdpList.gdpList.sortedWith(compareBy{it.name})
+
+        val letterArry = arrayOf(
+                "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+                "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+
+        var isInRange = false
+        var counter = 0
+        var rtnGDPArray = arrayListOf<GDP>()
+        var resultArry = mutableListOf<String>()
+        for((i, l:String) in letterArry.withIndex()){
+            when(l){
+                startLetter -> isInRange = true
+                endLetter -> isInRange = false
+            }
+
+            resultArry[counter] = letterArry[i]
+            if(isInRange){
+                counter++
+            }
+        }
+
+        for(g:GDP in sortedList){
+            if(resultArry.contains(g.name.substring(0,1))){rtnGDPArray.add(g)}
+        }
+
+        return ResponseEntity(rtnGDPArray, HttpStatus.OK)
+    }*/
+
+
     // localhost:2019/gdp/list/{startGdp}/{endGdp}
     @RequestMapping(value = "/gdp/list/{startGdp}/{endGdp}")
-    fun getByGdpRange(@PathVariable startGdp: Long, @PathVariable endGdp: Long):ResponseEntity<*>{
+    fun getByGdpRange(@PathVariable startGdp: Long, @PathVariable endGdp: Long):ResponseEntity<*> {
         logger.info("/gdp/list/$startGdp/$endGdp accessed")
-    }*/
+        var rtnGdpList = mutableListOf<GDP>()
+
+        for(g:GDP in ourGdpList.gdpList)
+        {
+            if(g.countryGdp in (startGdp + 1)..(endGdp - 1)){
+                rtnGdpList.add(g)
+            }
+        }
+        return ResponseEntity(rtnGdpList, HttpStatus.OK)
+    }
 }
